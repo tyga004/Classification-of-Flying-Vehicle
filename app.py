4b86fd7db9118b0a47511a0c844a56597e94a014
tyga004 <134521971+tyga004@users.noreply.github.com>
2023-05-24 22:26:50 +0800

    app.py

--- app.py
+++ app.py
@@ -6,15 +6,15 @@ import cv2
 
 def main():
     # set up the Streamlit app
-    st.write("Name: Joemart Gridson T. Turaray")
-    st.write("Section: CPE32S4")
-    st.write("Instructor: Dr. Jonathan Taylar")
-    st.title("Class Weather ( Sunrise/Cloudy)")
-    st.write("This app classifies whether an uploaded image contains a Sunrise or Cloudy images using a pre-trained convolutional neural network model.")
+    st.write("Name: Tyrone Brix R. Garraton")
+    st.write("Section: CPE32S2")
+    st.write("Instructor: Engr. Roman Richard")
+    st.title("Classifies Flying Vehicle ( Airplane/Helicopter)")
+    st.write("This app classifies whether an uploaded image contains a Airplane or Helicopter images.")
    
     @st.cache_resource
     def load_model():
-        model = tf.keras.models.load_model('weights-improvement-05-0.99.hdf5')
+        model = tf.keras.models.load_model('weights-improvement-10-0.98.hdf5')
         return model
     
     def import_and_predict(image_data, model):
@@ -27,10 +27,10 @@ def main():
         return prediction
 
     model = load_model()
-    class_names = ["CLOUDY", "SUNRISE"]
+    class_names = ["AIRPLANE", "HELICOPTER"]
     
 
-    file = st.file_uploader("Choose a Cloudy or Sunrise picture from your computer", type=["jpg", "png", "jpeg"])
+    file = st.file_uploader("Choose a Airplane or Helicopter picture from your computer", type=["jpg", "png", "jpeg"])
 
     if file is None:
         st.text("Please upload an image file")
